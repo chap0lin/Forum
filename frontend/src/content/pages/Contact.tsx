62ac6cf4442b72ef72d8a6f3bd642883ed67b66e
calmadogs <carlosjrdev23@gmail.com>
2026-02-18 18:54:05 -0300

    feat: botão flutuante do WhatsApp na page Fale Conosco

--- frontend/src/content/pages/Contact.tsx
+++ frontend/src/content/pages/Contact.tsx
@@ -1,16 +1,30 @@
 import ContactForm from "../components/ContactForm";
-import { Body, Container, Title, Card } from "./Contact.style";
+import { Body, Container, Title, Card, WhatsAppFloat } from "./Contact.style";
+import { WhatsappLogo } from "@phosphor-icons/react/dist/ssr";
+import Header from "../../shared/components/Header";
+import Footer from "../../shared/components/Footer";
 
 export default function Contact() {
   return (
     <Body>
+      <Header />
       <Container>
         <Title>Fale Conosco</Title>
 
         <Card>
           <ContactForm />
+
+          <WhatsAppFloat
+            href="https://wa.me/5561991124457"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <WhatsappLogo size={32} weight="fill" color="#fff" />
+          </WhatsAppFloat>
+
         </Card>
       </Container>
+      <Footer />
     </Body>
   );
 }
